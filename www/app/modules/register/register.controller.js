43f5eb7f969927b6f126aa84a091096838ab1617
RedParabola <secalero@ucm.es>
2018-01-02 21:35:41 +0100

    Register template is almost ready. Several css changes

--- www/app/modules/register/register.controller.js
+++ www/app/modules/register/register.controller.js
@@ -8,13 +8,13 @@ function registerController($rootScope, $log, UserService, $timeout) {
   var vm = this;
   vm.validateField = _validateField;
   vm.doRegister = _doRegister;    
-
+  
   initialize();
 
   //////////////////
 
   function initialize(){
-    
+    vm.isUserAnonymous = true;
   }
 
   function _validateField(){
