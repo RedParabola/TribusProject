43719a94a75df41e29f507fedcc411493acad8fb
RedParabola <secalero@ucm.es>
2018-01-03 22:40:01 +0100

    room creation wizard. step 1 almost finished, step 2 on the way, step 3 & 4 not started yet

--- www/app/modules/room-creation-wizard/room-creation-wizard.controller.js
+++ www/app/modules/room-creation-wizard/room-creation-wizard.controller.js
@@ -2,19 +2,63 @@ angular
 .module('starter.controllers')
 .controller('RoomCreationWizardController', roomCreationWizardController);
 
-roomCreationWizardController.$inject = ['$rootScope', '$scope', '$log', '$state'];
-function roomCreationWizardController($rootScope, $scope, $log, $state) {
+roomCreationWizardController.$inject = ['$rootScope', '$scope', '$log', '$state', 'CategoryConstants', 'PopupService'];
+function roomCreationWizardController($rootScope, $scope, $log, $state, CategoryConstants, PopupService) {
 
   var vm = this;
+  vm.addNewActor = _addNewActor;
+  vm.removeActor = _removeActor;
   var textToEncode = 'OMG! Tits';
   vm.makeCode = _makeCode;
+  vm.categories = [];
   
   initialize();
 
   //////////////////
 
   function initialize(){
+    vm.categories = CategoryConstants;
+    vm.actors = [
+      {
+        name:'SGAE',
+        sentence: 'Brief sentence.'
+      },
+      {
+        name:'NSA',
+        sentence: 'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM'
+      },
+      {
+        name:'NETFLIX',
+        sentence: 'Brief sentence about the actor. A bit longer.'
+      }
+    ];
+  }
 
+
+
+  function _addNewActor(){
+    //TODO: Insert new name/sentence
+    
+    var actorAlreadyExistent = _.find(vm.actors,function(actor){
+      return vm.newActorName = actor.name;
+    });
+    if(actorAlreadyExistent){
+      PopupService.getAlertPopup(
+        'Actor ' + vm.newActorName + ' already exists!',
+        'Please add another actor that is not already present in the list.'
+      );
+      vm.newActorName = undefined;
+      vm.newActorSentence = undefined;
+    } else {
+      vm.actors.push({
+        name: vm.newActorName,
+        sentence: vm.newActorSentence
+      });
+    }
+  }
+
+  function _removeActor(actor){
+    vm.actors = _.without(vm.actors,actor);
   }
 
   function _makeCode(){
