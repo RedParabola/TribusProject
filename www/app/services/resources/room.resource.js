898c20f9cb8b15ac439047e434a0acbd8f1effac
RedParabola <secalero@ucm.es>
2018-01-04 23:27:02 +0100

    Updated list of categories

--- www/app/services/resources/room.resource.js
+++ www/app/services/resources/room.resource.js
@@ -8,15 +8,25 @@ angular.module('starter.services')
     CREATEROOM: 'create'
   })
   .constant('CategoryConstants', [
-    {name: 'POLITICS', icon: 'pol-icon'},
-    {name: 'SPORTS', icon: 'pol-icon'},
-    {name: 'SOCIETY', icon: 'pol-icon'},
-    {name: 'TECHNOLOGY', icon: 'pol-icon'},
-    {name: 'PSYCHOLOGY', icon: 'pol-icon'},
-    {name: 'INTERNATIONAL', icon: 'pol-icon'}
+    {name: 'Culture',       icon: 'ion-android-color-palette'},
+    {name: 'Economics',     icon: 'ion-cash'},
+    {name: 'Education',     icon: 'ion-university'},
+    {name: 'Environment',   icon: 'ion-earth'},
+    {name: 'Health',        icon: 'ion-ios-heart'},
+    {name: 'History',       icon: 'ion-ios-world'},
+    {name: 'Human Rights',  icon: 'ion-android-hand'},
+    {name: 'Life Style',    icon: 'ion-paper-airplane'},    
+    {name: 'Moral',         icon: 'ion-ios-body-outline'},
+    {name: 'Politics',      icon: 'ion-ios-pie'},
+    {name: 'Psychology',    icon: 'ion-android-contacts'},
+    {name: 'Science',       icon: 'ion-ionic'},
+    {name: 'Society',       icon: 'ion-ios-people'},
+    {name: 'Sports',        icon: 'ion-ios-football'},
+    {name: 'Technology',    icon: 'ion-power'},
+    {name: 'Work',          icon: 'ion-ios-briefcase'}
   ]);
 
-  roomResource.$inject = ['ServerResource', 'SERVER_API_REST_CONSTANTS'];
+roomResource.$inject = ['ServerResource', 'SERVER_API_REST_CONSTANTS'];
 function roomResource(ServerResource, SERVER_API_REST_CONSTANTS) {
   return ServerResource.all(SERVER_API_REST_CONSTANTS.ROOM_URL).withHttpConfig({timeout: 1000});
 }
